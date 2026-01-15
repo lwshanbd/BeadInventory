46a622b556e99923ae0970306479ebc43576fe33
Baodi Shan <lwshanbd@gmail.com>
2026-01-15 18:00:52 -0500

    fix: 关于页面只显示版本号，不显示构建号

--- BeadInventory/Views/AboutView.swift
+++ BeadInventory/Views/AboutView.swift
@@ -12,8 +12,7 @@ struct AboutView: View {
 
     var appVersion: String {
         let version = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? "1.0"
-        let build = Bundle.main.infoDictionary?["CFBundleVersion"] as? String ?? "1"
-        return "版本 \(version) (\(build))"
+        return "版本 \(version)"
     }
 
     var body: some View {
